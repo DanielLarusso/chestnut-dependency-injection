74acf2685b2a26c8bed6cd3065d83fa2d210ea63
Daniel Pogodda <daniel.pogodda@transfermarkt.de>
2023-02-03 12:31:08 +0100

    add container builder class

---
+++ src/ContainerBuilder.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chestnut\DependencyInjection;
+
+class ContainerBuilder implements ContainerBuilderInterface
+{
+    public function add(string $key, array $options): static
+    {
+
+    }
+}
